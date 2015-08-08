ecc142cd29d29ba98c9844b9daaf278163e3d31f
kamo <kamo@punisher>
2015-08-07 23:57:44 -0700

    Added more code for refactor Task #1 - Add more filetype classes

--- src/com/EXIFCarver/EXIF.java
+++ src/com/EXIFCarver/EXIF.java
@@ -1,18 +1,42 @@
 package com.EXIFCarver;
 
-
-
 // Java imports
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-//import org.apache
+import com.EXIFCarver.FileTypes.*;
 
 public class EXIF
 {
+    // Priavte variables for the class
+    private File _file;
+    private Object _obj;
+    
+    public EXIF(String filename)
+    {
+        this.setFile(filename);
+        
+        // Create the needed object
+        String extension = getFileExtension(filename);
+                
+        //Class objcls = Class.forName(extension.toUpperCase()).newInstance();
+        
+        
+    }
+    
+    private String getFileExtension(String filename)
+    {        
+        return filename.substring(filename.lastIndexOf("."));
+    }
+    
+    public void setFile(String filename)
+    {
+       this._file = new File(filename);
+    }
+    
     private void setEXIFData() throws IOException
     {
-        File imgFile = null;
+        
     }
 }
--- src/com/EXIFCarver/FileTypes/PNG.java
+++ src/com/EXIFCarver/FileTypes/PNG.java
@@ -1,5 +1,6 @@
 package com.EXIFCarver.FileTypes;
 
+// Class for the .png extension
 public class PNG extends FileType
 {
     public PNG()
